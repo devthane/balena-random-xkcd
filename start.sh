6c9d26fcb2a55ad4eb7cd8ad5abde41f043a227e
Kaelbaldwin <kabaldwin@averittexpress.com>
2020-11-26 09:28:35 -0600

    disable suspend and DPMS

--- start.sh
+++ start.sh
@@ -3,9 +3,6 @@
 export DISPLAY=:0.0
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-xset s off
-xset -dpms
-
 # start desktop manager
 echo "STARTING X"
-startx /project/test -- -nocursor
+startx /project/test -- -nocursor -dpms -s 0
